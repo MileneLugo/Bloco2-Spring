ac38cf76f2616428a85833c76fca8b04d55aa7f6
MileneLugo <102683420+MileneLugo@users.noreply.github.com>
2022-04-28 22:23:09 -0300

    V1

---
+++ Blog/blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,59 @@
+package com.generation.blogpessoal.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+//transforma objeto em tabela
+@Entity
+
+//da o nome a tabela
+@Table(name= "tb_postagem")
+public class Postagem {
+	
+	//define coluna como chave primaria
+	@Id
+	//equivale ao auto_increment 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	
+	//campo deve ser preenchido
+	@NotNull
+	
+	//minimo e maximo de caracteres
+	@Size(min = 5, max = 100, message="O campo deve ter no minimo 5 caracteres e no maximo 100.")
+	public String titulo;
+	
+	@NotNull 
+	public String texto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	
+}
