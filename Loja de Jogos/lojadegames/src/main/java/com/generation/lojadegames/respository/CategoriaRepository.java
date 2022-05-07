24ddcfc366bb1d4f211e2e8b8347abe95dc81366
MileneLugo <102683420+MileneLugo@users.noreply.github.com>
2022-05-07 14:08:49 -0300

    V3

---
+++ Loja de Jogos/lojadegames/src/main/java/com/generation/lojadegames/respository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.generation.lojadegames.respository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.lojadegames.model.Categoria;
+
+	@Repository
+	public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+		public List <Categoria> findAllByDescricaoContainingIgnoreCase (String descricao);
+
+	}
